c48c493ee3ecd5ec456f872d35f651bd68b65c83
Poliklot <poliklot@vk.com>
2026-09-09 17:02:26 +0300

    docs: clarify Google Play feedback options

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -3,8 +3,8 @@ import type { APIRoute } from 'astro';
 const pages = [
 	{url: 'https://poliklot.ru/', language: 'ru', alternates: true, lastmod: '2026-09-08'},
 	{url: 'https://poliklot.ru/en/', language: 'en', alternates: true, lastmod: '2026-09-08'},
-	{url: 'https://poliklot.ru/articles/', language: 'ru', alternates: false, lastmod: '2026-09-08'},
-	{url: 'https://poliklot.ru/articles/mobile-app-testing/', language: 'ru', alternates: false, lastmod: '2026-09-08'},
+	{url: 'https://poliklot.ru/articles/', language: 'ru', alternates: false, lastmod: '2026-09-09'},
+	{url: 'https://poliklot.ru/articles/mobile-app-testing/', language: 'ru', alternates: false, lastmod: '2026-09-09'},
 ];
 
 const alternates = pages
